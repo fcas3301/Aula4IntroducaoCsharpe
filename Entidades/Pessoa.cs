85888f7878d6c531efe67bf193dcdf8d629dcf63
Fidel Castro <fidelcastroas@yahoo.com.br>
2023-01-15 11:21:37 -0300

    Inicio de Projeto

---
+++ Entidades/Pessoa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExempleConsoleTemplate.Entidades
+{
+    internal class Pessoa
+    {
+        
+
+      
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using ExempleConsoleTemplate.Entidades;
+
+namespace ExempleConsoleTemplate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa = 1;
+
+        }
+    }
+}
